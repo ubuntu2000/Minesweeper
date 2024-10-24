c30189a57dce2b90a4f11ba30e727b6b0db4a7e9
cunghocnao89 <hongbien1989hp@gmail.com>
2024-10-24 23:17:04 +0700

    Update v1.01

--- Assets/Scripts/Cell.cs
+++ Assets/Scripts/Cell.cs
@@ -1,18 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
+﻿
 using UnityEngine;
 
-public class Cell : MonoBehaviour
+public struct Cell 
 {
-    // Start is called before the first frame update
-    void Start()
+    // Các kiểu Thuộc tính ô cell như:  kiểu rỗng, mìn, số
+   public enum Type
     {
-        
+        Empty,
+        Mine,
+        Number,
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    // Vị trí của ô cell biểu diễn bằng số nguyên( tọa độ x,y,z là số nguyên)
+    public Vector3Int position;
+    // Thuộc tính ô cell cụ thể
+    public Type type;
+    // Thuộc tính number của ô cell thuộc kiểu dữ liệu int( khai báo dữ liệu thuộc tính number)
+    public int number;
+    // kiểm tra trạng thái ô được nhấp vào hay chưa
+    public bool revealed;
+    // kiểm tra trạng thái ô gắn cờ hay chưa
+    public bool flagaged;
+    // Kiểm tra ô đã nổ hay chưa
+    public bool exploded;
 }
